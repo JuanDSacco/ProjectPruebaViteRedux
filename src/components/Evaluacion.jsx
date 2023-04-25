83b70fe1137a2ddaba22c70b9597e939fdaf92ae
juan sacco <juandsacco@gmail.com>
2023-04-25 00:58:12 -0300

    modif

--- src/components/Evaluacion.jsx
+++ src/components/Evaluacion.jsx
@@ -7,16 +7,21 @@ const Evaluacion = () => {
     const dispatch = useDispatch();
     
     return(
-        <div>
-            <h1>Ingresa el valor de tu nota: </h1>
-        <div>
-            {evaluacion}
-        </div>
-            <button onClick={() => dispatch(excelente())}>10-9</button>
-            <button onClick={() => dispatch(regular())}>8-7</button>
-            <button onClick={() => dispatch(desaprobado())}>6-5-4</button>
-            <button onClick={() => dispatch(aplazo())}>3-2-1</button>
-        </div>
+        <>
+            <div>
+                <h1>Ingresa el valor de tu nota: </h1>
+            <div>
+                {evaluacion}
+            </div>
+                <button onClick={() => dispatch(excelente())}>10-9</button>
+                <button onClick={() => dispatch(regular())}>8-7</button>
+                <button onClick={() => dispatch(desaprobado())}>6-5-4</button>
+                <button onClick={() => dispatch(aplazo())}>3-2-1</button>
+            </div>
+            <div>
+                <h4>para resolver el problema tendria que usar useState para poder modificar el estado del componente luego de switchear? o en este caso deberia usar un condicional en vez de un ciclo?</h4>
+            </div>
+        </>
     )
 }
 
